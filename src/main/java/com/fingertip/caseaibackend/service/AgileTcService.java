88162d4988a9e05474161c313659c443aa4c4eaa
CuiingJing <cuijing@leoao.com>
2025-08-15 10:09:41 +0800

    code commit

--- src/main/java/com/fingertip/caseaibackend/service/AgileTcService.java
+++
@@ -1,9 +0,0 @@
-package com.fingertip.caseaibackend.service;
-
-import com.fingertip.caseaibackend.entity.MindMap;
-import com.fingertip.caseaibackend.vo.ApiResult;
-
-public interface AgileTcService {
-    public ApiResult<Boolean> saveToAgileTc(String kmData, String caseName, ApiResult<Boolean> result);
-    public MindMap convertMarkdownToKityMinder(String markdown);
-}
